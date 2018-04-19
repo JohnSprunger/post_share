19cfa2d7c93abee2a4b16526e7248069d2089c97
John Sprunger <johnleesprunger@gmail.com>
2018-04-19 13:21:09 -0400

    Added Login page markup

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -19,11 +19,30 @@ class Users extends Controller{
                 'name_err' => '',
                 'emali_err' => '',
                 'password_err' => '',
-                'confirm _passsword' => ''
+                'confirm_passsword_err' => ''
             ];
 
             // Load view
             $this->view('users/register', $data);
+        }
+    }
+
+    public function login(){
+        // Check for POST
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            // Process form
+        }
+        else{
+            // Init data
+            $data =[
+                'email' => '',
+                'password' => '',
+                'emali_err' => '',
+                'password_err' => '',
+            ];
+
+            // Load view
+            $this->view('users/login', $data);
         }
     }
 }
