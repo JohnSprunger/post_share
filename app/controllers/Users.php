896484f64fe8c84ecc178b79580e8a8c08d2469f
John Sprunger <johnleesprunger@gmail.com>
2018-04-23 01:43:28 -0400

    Email verification

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -27,6 +27,11 @@
         // Validate Email
         if(empty($data['email'])){
           $data['email_err'] = 'Pleae enter email';
+        } else {
+          // Check email
+          if($this->userModel->findUserByEmail($data['email'])){
+            $data['email_err'] = 'Email is already taken';
+          }
         }
 
         // Validate Name
