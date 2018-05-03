1696304acc7b58b3cfa335c17ae06036e19172d7
John Sprunger <johnleesprunger@gmail.com>
2018-05-03 12:32:18 -0400

    Added display for posts

--- app/models/Post.php
+++ app/models/Post.php
@@ -1,5 +1,5 @@
 <?php
-  class Post {
+class Post {
     private $db;
 
     public function __construct(){
@@ -7,10 +7,19 @@
     }
 
     public function getPosts(){
-        $this->db->query('SELECT * FROM posts');
+        $this->db->query('SELECT *,
+                          posts.id as postId, 
+                          users.id as userID,
+                          posts.created_at as postCreated, 
+                          users.created_at as usersCreated
+                          FROM posts
+                          INNER JOIN users
+                          ON post.user_id = users.id
+                          ORDER BY post.created_at DESC
+                          ');
 
-        $results = $this->db-resultSet();
+        $results = $this->db->resultSet();
 
         return $results;
     }
-  }
+}
