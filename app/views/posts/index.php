022d1a9fe0d685a7246eea5d680305ee2da3f1a1
John Sprunger <johnleesprunger@gmail.com>
2018-05-03 11:30:17 -0400

    Added Posts controller and it's index view

---
+++ app/views/posts/index.php
@@ -0,0 +1,13 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+    <div class="row">
+        <div class="col-md-6">
+            <h1>Posts</h1>
+        </div>
+        <div class="col-md-6">
+            <a href="<?php echo URLROOT; ?>/post/add" class="btn btn-primary pull-right">
+                <i class="fa fa-pencil"></i> Add Post
+            </a>
+
+        </div>
+    </div>
+<?php require APPROOT . '/views/inc/footer.php'; ?>
